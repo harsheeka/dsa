1ad987920286d0f7832afc3b0b5bbe17738f2f94
Harshika Arya <120313378+harsheeka@users.noreply.github.com>
2025-07-21 21:41:48 +0530

    Time: 0 ms (100%), Space: 7.9 MB (86.59%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int countPath(int m, int n){
-        int N = (m + n - 2);      // total moves
-        int R = min(m - 1, n - 1); // choose min steps
+        int N = (m + n - 2);   
+        int R = min(m - 1, n - 1); 
 
         long double result = 1.0;
         for (int i = 1; i <= R; i++) {
             result *= static_cast<long double>(N - R + i) / i;
         }
 
-        return static_cast<int>(round(result)); // rounding helps avoid float truncation
+        return static_cast<int>(round(result));
     }
 
     int uniquePaths(int m, int n) {
