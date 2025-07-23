479704d8a07c001ad82250bd2eb1e9b784d1258e
Harshika Arya <120313378+harsheeka@users.noreply.github.com>
2025-07-24 00:57:39 +0530

    Time: 7 ms (23.45%), Space: 15.8 MB (15.05%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isSafe(int &i, int &j, vector<string> &temp, int &n){
+        for(int y = i-1; y>=0;y--){
+            if(temp[y][j]=='Q') return false;
+        }
+        for(int x = i-1, y= j-1; x>=0 && y>=0; x--,y--){
+            if(temp[x][y]=='Q') return false;
+        }
+        for(int x = i-1, y= j+1; x>=0 && y<n; x--,y++){
+            if(temp[x][y]=='Q') return false;
+        }
+        return true;
+    }
+    void res(vector<vector<string>> &result, vector<string> temp , int row, int &n){
+        if(row==n){
+            result.push_back(temp);
+            return;
+        }
+        for(int col=0;col<n;col++){
+            temp[row][col]='Q';
+            if(isSafe(row,col,temp,n)){
+                res(result,temp,row+1,n);
+            }
+            temp[row][col]='.';
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> temp(n, string(n,'.')) ;
+        vector<vector<string>> result;
+        res(result,temp,0,n);
+        return result;
+    }
+};
