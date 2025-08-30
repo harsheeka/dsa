dbe3c6f0da8c1415d30c8ee7fd4d29a564abf996
Harshika Arya <120313378+harsheeka@users.noreply.github.com>
2025-08-31 02:56:13 +0530

    Time: 14 ms (77.21%), Space: 45.7 MB (69.62%) - LeetHub

---
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int i=0,j=people.size()-1,count=0;
+        while(i<j){
+            int sum = people[i]+people[j];
+            if(sum<=limit){
+                count++;
+                i++;
+                j--;
+            }
+            else{
+              count++;
+              j--;  
+            }
+        }
+        return i==j ? count+1: count;
+    }
+    
+};
